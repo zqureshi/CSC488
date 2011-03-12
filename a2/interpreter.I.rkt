ade202f7547df7cb092fec2205dac3a53c471704
Zeeshan Qureshi <zeeshan@zqureshi.in>
2011-03-11 20:58:19 -0500

    basic compiler infrastructure works

--- a2/interpreter.I.rkt
+++ a2/interpreter.I.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 #| Part I: Completely Manual Interpreter. |#
-(provide #;interpret Binding)
+(provide interpret Binding)
 
 #| Recall the language and IR from A1 Parts II and III.
 
@@ -28,8 +28,35 @@ Write 'interpret' taking such code as a value/data,
  i.e. you're working on the "Abstract Syntax Tree", not
   an unparsed string of characters.
 It also takes an environment, as a list of 'Binding's. |#
-(struct Binding (id (value #:mutable)))
-#;(define (interpret exp env))
+(struct Binding (id (value #:mutable)) #:transparent)
+(struct Closure (environment parameters body) #:transparent)
+
+#|  Helper to locate specific binding |#
+(define (find-binding env id)
+  (findf (λ (binding)
+           (equal? (Binding-id binding) id))
+         env))
+
+(define (LOOKUP env id)
+  (Binding-value (find-binding env id)))
+
+(define (UPDATE! env id val)
+  (set-Binding-value! (find-binding env id) val))
+
+(define (interpret exp env)
+  (match exp
+    [`(LOOKUP ,var) (LOOKUP env var)]
+    [`(UPDATE! ,var ,expr) (let ([val (interpret expr env)])
+                            (UPDATE! env var val))]
+    [`(CLOSURE (,param ...) ,body ...) (Closure env param body)]
+    [`(CALL ,func-expr . ,args-expr) (let* ([clojure (interpret func-expr env)]
+                                            [args (map (λ (expr) (interpret expr env)) args-expr)]
+                                            [env (append
+                                                      (map Binding (Closure-parameters clojure) args)
+                                                      (Closure-environment clojure))])
+                                       (foldl (λ (stmt prev) (interpret stmt env)) 
+                                              (void) (Closure-body clojure)))]
+    [atomic-literal atomic-literal]))
 
 #| Approach: you are now managing code traversal, closure code
     and environments. In particular, don't use 'eval', nor try
